158ef5e4d4287cc078c59e535925a81fb3dcb218
Chris Laughlin <cjlaughlin@gmail.com>
2016-03-30 18:45:44 -0700

    added more words

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello, chris!');
+  res.send('hello, chris! this is cool!');
 });
 
 app.listen(process.env.PORT || 3000);
